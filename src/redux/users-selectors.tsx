ff9be6d78aeda2b70f02460e0d90884238108445
mskuratova <m.p.skuratova@gmail.com>
2021-12-08 16:33:49 +0600

    83

--- src/redux/users-selectors.tsx
+++ src/redux/users-selectors.tsx
@@ -1,8 +1,14 @@
 import {AppStateType} from "./redux-store";
+import {createSelector} from "reselect";
 
-export const getUsers = (state: AppStateType) => {
+const getUsersSelector = (state: AppStateType) => {
     return state.usersPage.users;
 }
+export const getUser = createSelector (
+    getUsersSelector, (users) => {
+        return users.filter(u => true)
+    }
+)
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize;
 }
