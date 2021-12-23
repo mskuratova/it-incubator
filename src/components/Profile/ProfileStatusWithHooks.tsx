6a6f3e98cdf121d6c88c98e362eac69be1c5756e
mskuratova <m.p.skuratova@gmail.com>
2021-12-23 13:59:55 +0600

    85

--- src/components/Profile/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
  type PropsType = {
      status: string,
      updateStatus: (status:string) => void
@@ -7,6 +7,10 @@ const ProfileStatusWithHooks = (props: PropsType) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true);
